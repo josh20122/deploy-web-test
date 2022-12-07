5f58ae4977d866d67fb65ff10e675e144ddad0f4
josh20122 <jamesjoshua20122@gmail.com>
2022-12-07 08:13:42 +0300

    remove pagea

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Web-test/',
+  base:'/deploy-web-test/',
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
